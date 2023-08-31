20235cf0ab8639a0328bf6f60177439364bf6639
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-08-31 09:59:54 +0800

    :sparkles: Added URL Launcher

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 final monthMap = {
   1: 'January',
@@ -28,12 +29,17 @@ class _DashboardState extends State<Dashboard> {
 
   Future<bool> isDataAvailable(int year, int month) async {
     var res = await get(Uri.parse(
-        'https://corsproxyuia.up.railway.app/solatapi.iqfareez.com/api/v2/solat/ngs02?year=$year&month=$month'));
+        'https://solatapi.iqfareez.com/api/v2/solat/ngs02?year=$year&month=$month'));
     return res.statusCode == 200;
   }
 
   int getAxisCount() {
-    if (MediaQuery.orientationOf(context) == Orientation.portrait) {
+    final screenWidth = MediaQuery.sizeOf(context).width;
+    if (screenWidth <= 280) {
+      return 1;
+    } else if (screenWidth < 420) {
+      return 2;
+    } else if (screenWidth < 821) {
       return 3;
     } else {
       return 6;
@@ -43,75 +49,86 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('MPT Health Dashboard'),
-        ),
-        body: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton.outlined(
-                  onPressed: currentYear < 2023
-                      ? null
-                      : () => setState(() => currentYear--),
-                  icon: const Icon(Icons.chevron_left),
-                ),
-                Text(
-                  currentYear.toString(),
-                  style: Theme.of(context).textTheme.headlineLarge,
-                ),
-                IconButton.outlined(
-                  onPressed: () => setState(() => currentYear++),
-                  icon: const Icon(Icons.chevron_right),
-                ),
-              ],
-            ),
-            GridView.builder(
-              padding: const EdgeInsets.all(24),
-              itemCount: 12,
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: getAxisCount(), childAspectRatio: 3 / 2),
-              itemBuilder: (context, index) {
-                var month = index + 1;
-                var monthName = monthMap[month];
-                return Card(
-                  child: FutureBuilder(
-                      future: isDataAvailable(currentYear, month),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        }
-                        if (snapshot.hasError) {
-                          return Text(
-                            'Error',
-                            style: Theme.of(context).textTheme.headline5,
-                          );
-                        }
-                        return Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              snapshot.data! ? Icons.check : Icons.close,
-                              color: snapshot.data! ? Colors.green : Colors.red,
-                              size: 56,
-                            ),
-                            Text(
-                              monthName!,
-                              style: Theme.of(context).textTheme.headlineSmall,
-                            ),
-                          ],
-                        );
-                      }),
-                );
+      appBar: AppBar(
+        title: const Text('MPT Health Dashboard'),
+        actions: [
+          TextButton(
+              onPressed: () {
+                launchUrl(Uri.parse('https://mpt-server.vercel.app'));
               },
-            ),
-          ],
-        ));
+              child: const Text('MPT-Server')),
+        ],
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(24),
+        children: [
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton.outlined(
+                onPressed: currentYear < 2023
+                    ? null
+                    : () => setState(() => currentYear--),
+                icon: const Icon(Icons.chevron_left),
+              ),
+              Text(
+                currentYear.toString(),
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              IconButton.outlined(
+                onPressed: () => setState(() => currentYear++),
+                icon: const Icon(Icons.chevron_right),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          GridView.builder(
+            itemCount: 12,
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: getAxisCount(), childAspectRatio: 3 / 2),
+            itemBuilder: (context, index) {
+              var month = index + 1;
+              var monthName = monthMap[month];
+              return Card(
+                child: FutureBuilder(
+                    future: isDataAvailable(currentYear, month),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      if (snapshot.hasError) {
+                        return Text(
+                          'Error',
+                          style: Theme.of(context).textTheme.headline5,
+                        );
+                      }
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            snapshot.data! ? Icons.check : Icons.close,
+                            color: snapshot.data! ? Colors.green : Colors.red,
+                            size: 56,
+                          ),
+                          Text(
+                            monthName!,
+                            style: Theme.of(context).textTheme.headlineSmall,
+                          ),
+                        ],
+                      );
+                    }),
+              );
+            },
+          ),
+          const SizedBox(height: 10),
+          const Text('All data is check based on NGS02 zone')
+        ],
+      ),
+    );
   }
 }
